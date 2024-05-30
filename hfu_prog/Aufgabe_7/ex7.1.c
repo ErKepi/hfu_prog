c4b50796d029f4c440b671ad16131c583087bcde
ErKepi <ipek.erkan97@gmail.com>
2024-05-30 18:29:10 +0200

    init

---
+++ hfu_prog/Aufgabe_7/ex7.1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+void zp(int a, int b) {
+    //int array[b];
+
+    if (a < 0 || b <= 0 || b < a) {
+        printf("Fehlerhafte Eingaben!\n");
+    } else {
+        //Inner scope
+        int powTwo=0;
+        int n = 0;
+
+        while (powTwo <= b) {
+            powTwo = pow(2, n);
+            n++;
+            if (powTwo >= a && powTwo <= b) {
+                printf("%i ", powTwo);
+            }
+        }
+    }
+}
+
+int main() {
+    int a, b;
+
+    printf("Geben Sie den ersten Wert an: ");
+    scanf("%i", &a);
+
+    printf("Geben Sie den zweiten Wert an: ");
+    scanf("%i", &b);
+
+    // printf("%i\n", pow(a, b));
+
+    zp(a, b);
+
+    return 0;
+}
